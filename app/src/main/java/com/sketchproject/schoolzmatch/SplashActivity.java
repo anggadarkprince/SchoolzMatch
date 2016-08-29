242a962d4609c157f4b5285ce1e82507c15133b7
Angga Ari Wijaya <anggadarkprince@gmail.com>
2016-08-29 20:57:42 +0700

    add assets and create splash screen

--- app/src/main/java/com/sketchproject/schoolzmatch/SplashActivity.java
+++ app/src/main/java/com/sketchproject/schoolzmatch/SplashActivity.java
@@ -1,13 +1,24 @@
 package com.sketchproject.schoolzmatch;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
 import android.os.Bundle;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        final int SPLASH_TIME_OUT = 3000;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent applicationIntent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(applicationIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }

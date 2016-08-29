41113e8cc15933af0bab175c5e77d6b947d48d8d
Angga Ari Wijaya <anggadarkprince@gmail.com>
2016-08-29 22:47:27 +0700

    add main menu screen

--- app/src/main/java/com/sketchproject/schoolzmatch/MainActivity.java
+++ app/src/main/java/com/sketchproject/schoolzmatch/MainActivity.java
@@ -1,7 +1,12 @@
 package com.sketchproject.schoolzmatch;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.ContextThemeWrapper;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,56 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonProfile = (Button) findViewById(R.id.buttonProfile);
+        assert buttonProfile != null;
+        buttonProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        Button buttonSchedule = (Button) findViewById(R.id.buttonSchedule);
+        assert buttonSchedule != null;
+        buttonSchedule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        Button buttonAbout = (Button) findViewById(R.id.buttonAbout);
+        assert buttonAbout != null;
+        buttonAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        Button buttonExit = (Button) findViewById(R.id.buttonExit);
+        assert buttonExit != null;
+        buttonExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(MainActivity.this, R.style.AppTheme_AlertDialogStyle));
+                builder.setTitle("Confirm Exit App");
+                builder.setMessage("Are you sure want to exit app? the alarm still active in background");
+                builder.setPositiveButton("Exit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.show();
+            }
+        });
     }
 }
